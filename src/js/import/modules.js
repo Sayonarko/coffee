6364224cd39f9733bb6ab0c73a76a4812322da1a
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-02-08 19:39:32 +0200

    add swiper

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,2 +1,12 @@
-import "%modules%/header/header";
-import "%modules%/footer/footer"
+import Swiper from "swiper";
+
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 1000,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+});
