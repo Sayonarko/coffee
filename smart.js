854fed1c4cb527faded709d49c76178bab4b06e5
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-02-11 00:29:22 +0200

    finally update

--- smart.js
+++
@@ -1,7 +0,0 @@
-const smartGrid = require('smart-grid');
- 
-const options = {
-   /* your grid settings here */
-};
- 
-smartGrid('src/styles/base', options);
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,36 @@
-import "./import/modules";
+import $ from "jquery";
+$(window).on("load", function () {
+    //modal window
+    function openCloseModal() {
+        $(".js-modal-container").toggleClass("hidden");
+        $("body").toggleClass("disable");
+    }
+    $(".js-message").on("click", openCloseModal);
+
+    $(".js-modal-container").on("click", function (e) {
+        (e.target == $(this)[0]) ? openCloseModal() : null;
+    });
+
+    $(".js-form-btn").on("click", function (e) {
+        e.preventDefault();
+        $(".js-form").addClass("hidden");
+        $(".js-done").removeClass("hidden");
+        setTimeout(openCloseModal, 2000);
+    });
+
+    $(".js-modal-close").on("click", openCloseModal);
+
+    //swiper
+    const swiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: {
+            delay: 4000,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+    });
+});
+
