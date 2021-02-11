fa93baa5db7fc5eb8fba7471ece7e6ed23fd641e
Sayonarko <35679428+Sayonarko@users.noreply.github.com>
2021-02-11 17:08:57 +0200

    add multi-language

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,53 @@
 import $ from "jquery";
 $(window).on("load", function () {
     //modal window
-    function openCloseModal() {
-        $(".js-modal-container").toggleClass("hidden");
-        $("body").toggleClass("disable");
+    function openModal() {
+        $(".js-modal-container").fadeIn(500).css('display', 'flex');
+        $("body").addClass("disable");
     }
-    $(".js-message").on("click", openCloseModal);
+
+    function closeModal() {
+        $(".js-modal-container").fadeOut(500);
+        $("body").removeClass("disable");
+    }
+    $(".js-message").on("click", openModal);
 
     $(".js-modal-container").on("click", function (e) {
-        (e.target == $(this)[0]) ? openCloseModal() : null;
+        (e.target == $(this)[0]) ? closeModal() : null;
     });
 
     $(".js-form-btn").on("click", function (e) {
         e.preventDefault();
         $(".js-form").addClass("hidden");
         $(".js-done").removeClass("hidden");
-        setTimeout(openCloseModal, 2000);
+        setTimeout(closeModal, 2000);
     });
 
-    $(".js-modal-close").on("click", openCloseModal);
+    $(".js-modal-close").on("click", closeModal);
 });
 
-    //swiper
-    import Swiper, {Pagination, Navigation, Autoplay} from "swiper";
-    Swiper.use([Navigation, Pagination, Autoplay]);
-    const swiper = new Swiper('.swiper-container', {
-        loop: true,
-        autoplay: {
-            delay: 4000,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
-        },
-    });
+//swiper
+import Swiper, { Pagination, Navigation, Autoplay } from "swiper";
+Swiper.use([Navigation, Pagination, Autoplay]);
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 4000,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+});
+
+//lang select
+var lang = document.getElementById('lang');
+lang.selectedIndex = localStorage.getItem("index") ? localStorage.getItem("index") : 0;
+lang.options[lang.selectedIndex].setAttribute('disabled', 'disabled');
+
+lang.onchange = function () {
+    localStorage.setItem("index", lang.selectedIndex);
+    var language = (lang.selectedIndex === 0) ? "/" : (lang.selectedIndex === 1) ? "ru.html" : "ua.html";
+    window.location = language;
+}
